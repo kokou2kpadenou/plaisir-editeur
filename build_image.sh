9171cccb7035b7a54d7687568ffb9c192cf029d9
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-10-27 23:00:05 -0400

    Start using other password than the username

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+# TODO: Take password from console
 # buid the image
-docker build --build-arg user=$USER --build-arg uid=$(id -u) --build-arg gid=$(id -g) -t saint .
+docker build --build-arg user=$USER --build-arg password="djdj" --build-arg uid=$(id -u) --build-arg gid=$(id -g) -t saint .
