2ce67af006a344929611d37b965cbe5ba3dbd7af
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-10-29 16:27:57 -0400

    Prompt to root password

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-# TODO: Take password from console
+# Color
+RED='\033[0;31m'
+NC='\033[0m'  # No Color#
+
+echo "Creation of dnvim (neovim container) image"
+echo "------------------------------------------"
+echo
+
+password=""
+password2="2"
+
+while [[ "$password" = "" || "$password" != "$password2" ]]; do
+  read -s -p "Enter root password: " password
+  echo
+  read -s -p "Confirm root password: " password2
+  echo
+  [[ "$password" = "" ]] && echo -e "${RED}User password cannot be empty${NC}" || \
+  [[ "$password" == "$password2" ]] || echo -e "${RED}Passwords did not match${NC}"
+done
+
+
 # buid the image
-docker build --build-arg user=$USER --build-arg password="djdj" --build-arg uid=$(id -u) --build-arg gid=$(id -g) -t saint .
+docker build --build-arg user=$USER --build-arg password="${password}" --build-arg uid=$(id -u) --build-arg gid=$(id -g) -t saint .
